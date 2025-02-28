b79a0db93611066dd27e5f537fddd6523901d2d0
MongoDev <149850427+mongus82@users.noreply.github.com>
2025-02-28 09:56:31 -0300

    cambie nombre world

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("Hello World")
+st.write("Hello Wrld")
 st.write("## This is a H2 Title!")
 x = st.text_input("Movie", "Star Wars")
 
